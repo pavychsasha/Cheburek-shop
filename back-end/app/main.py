a3cb167dda519c36add7b632c8ea1c1070748ecc
pavychsasha <sashpavych@gmail.com>
2024-09-09 11:30:52 -0500

    BE-3 - adding CORS middelware for the react containerOC

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -22,7 +22,8 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan, default_response_class=ORJSONResponse)
 
 origins = [
-    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://react-container:5173",
 ]
 
 app.add_middleware(
