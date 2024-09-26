68b331e3d2dbcfaf3e7fc109b31abb88e8ba4d83
pavychsasha <sashpavych@gmail.com>
2024-09-26 11:46:57 -0500

    BE-8 - switching to CookieTransport

--- back-end/app/api/dependencies/authentication/backend.py
+++ back-end/app/api/dependencies/authentication/backend.py
@@ -1,10 +1,13 @@
 from fastapi_users.authentication import AuthenticationBackend
-from app.api.dependencies.authentication.transport import bearer_transport
+from app.api.dependencies.authentication.transport import (
+    # bearer_transport,
+    cookie_transport,
+)
 from app.api.dependencies.authentication.strategy import get_database_strategy
 
 
 auth_backend = AuthenticationBackend(
     name="access-tokens-db",
-    transport=bearer_transport,
+    transport=cookie_transport,
     get_strategy=get_database_strategy,
 )
