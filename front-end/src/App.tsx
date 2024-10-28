807b83294e5b5b500cc21e74fa37e04302d33515
Ostap Kravets <162045558+ost1qq@users.noreply.github.com>
2024-10-28 15:50:12 +0200

    CS-13 Reworked handling of /cart/ requests. (#55)

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,26 +1,31 @@
 import './App.scss';
 import Header from "./components/common/Header/Header.tsx";
 import Home from "./components/pages/Home/Home.tsx";
-import { Route, Routes, useLocation } from "react-router-dom";
+import {Route, Routes, useLocation} from "react-router-dom";
 import NotFound from "./components/pages/NotFound/NotFound.tsx";
 import Cart from "./components/pages/Cart/Cart.tsx";
 import Login from "./components/pages/Auth/Login/Login.tsx";
 import Register from "./components/pages/Auth/Register/Register.tsx";
-import { useDispatch } from "react-redux";
+import {useDispatch} from "react-redux";
 import React from "react";
-import { fetchCart } from "./redux/slices/cartSLice.ts";
-import { useAuthCheck } from "./hooks/useAuthCheck";
+import {fetchCart} from "./redux/slices/cartSLice.ts";
+import axios from "axios";
+import {setIsAuth} from "./redux/slices/authSlice.ts";
 
 const App = () => {
     const dispatch = useDispatch();
 
-    const isAuthorized = useAuthCheck();
+    React.useEffect(() => {
+        axios.get('http://localhost:8000/api/v1/users/me', {withCredentials: true}).then(response => {
+            if (response.status === 200) {
+                setIsAuth(true);
+            }
+        })
+    }, [])
 
     React.useEffect(() => {
-        if (isAuthorized) {
-            dispatch(fetchCart());
-        }
-    }, [dispatch, isAuthorized]);
+        dispatch(fetchCart());
+    }, [dispatch]);
 
     const location = useLocation();
 
@@ -28,13 +33,13 @@ const App = () => {
 
     return (
         <div className={shouldShowHeader ? 'app__wrapper' : ''}>
-            {shouldShowHeader && <Header />}
+            {shouldShowHeader && <Header/>}
             <Routes>
-                <Route path={'/'} element={<Home />} />
-                <Route path={'/cart'} element={<Cart />} />
-                <Route path={'/login'} element={<Login />} />
-                <Route path={'/register'} element={<Register />} />
-                <Route path={'*'} element={<NotFound />} />
+                <Route path={'/'} element={<Home/>}/>
+                <Route path={'/cart'} element={<Cart/>}/>
+                <Route path={'/login'} element={<Login/>}/>
+                <Route path={'/register'} element={<Register/>}/>
+                <Route path={'*'} element={<NotFound/>}/>
             </Routes>
         </div>
     );
